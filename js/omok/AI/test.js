8742edc436b266a3491b0d1fcde1f55b4c4234eb
tmdgus0084 <32220765+tmdgus0084@users.noreply.github.com>
2019-02-23 09:18:17 +0900

    오목 애니메이션 개선
    오목 애니메이션의 현재 상태를 알리는 설명글을 추가, 무승부시 자동 재경기.

--- js/omok/AI/test.js
+++ js/omok/AI/test.js
@@ -1,12 +1,29 @@
 //AI의 테스트를 위해, AI vs AI 대결을 시키는 함수.
 game.AIvsAI = async () => {
-  let i = 1;
-  const key = game.AItestKey = Math.floor(Math.random() * 100);
+  const explain = $('#omok-explain');
+  let i = 1,
+      winner;
+
+  if (game.AItestKey) return;
+  game.AItestKey = 1;
+  explain.innerHTML = '대결중...';
   game.stone.reset();
-  while (!game.checkWin()) {
-    if (key !== game.AItestKey) return;
-    game.stone.set(i + 1, ...AI(i + 1, game.stone.list));
-    i = 1 - i;
+
+  while (!(winner = game.checkWin())) {
+    try {
+      game.stone.set(i + 1, ...AI(i + 1, game.stone.list));
+      i = 1 - i;
+    } catch (e) {
+      i = 1;
+      game.stone.reset();
+      explain.innerHTML = '오목판이 가득 차서 재대결합니다.';
+      await wait(1500);
+      explain.innerHTML = '대결중...';
+    }
+
     await wait(1);
   }
+
+  explain.innerHTML = `${winner === BLACK? '흑' : '백'}돌 승리!`;
+  game.AItestKey = 0;
 }
